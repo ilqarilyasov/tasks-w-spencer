96911da913559a0f2e96fa751102c5c39e040b94
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-25 23:20:01 -0500

    TasksTableViewController

--- TasksSC/View Controller/TaskDetailViewController.swift
+++ TasksSC/View Controller/TaskDetailViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class TaskDetailViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    var taskController: TaskController?
+    var task: Task?
 
     // MARK: - Outlets
     

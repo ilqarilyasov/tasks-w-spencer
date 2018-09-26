9a69779eb9f5d4148c5ae04db6aec124229edfa3
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-26 17:08:30 -0500

    put function completed

--- TasksSC/Model Controller/TaskController.swift
+++ TasksSC/Model Controller/TaskController.swift
@@ -11,12 +11,17 @@ import CoreData
 
 class TaskController {
     
+    init() {
+        fetchTasksFromServer()
+    }
+    
     // MARK: - CRUD
     
     func createTask(with name: String, notes: String?, priority: TaskPriority) {
-        _ = Task(name: name, notes: notes, priority: priority)
+        let task = Task(name: name, notes: notes, priority: priority)
         
         saveToPersistentStore()
+        put(task: task)
     }
     
     func updateTask(task: Task, name: String, notes: String?, priority: TaskPriority) {
@@ -25,6 +30,7 @@ class TaskController {
         task.priority = priority.rawValue
         
         saveToPersistentStore()
+        put(task: task)
     }
     
     func deleteTask(task: Task) {
@@ -42,14 +48,36 @@ class TaskController {
             NSLog("Error saving moc: \(error)")
         }
     }
+    
+    
+    // MARK: - Find task by identifier
+    
+    func task(for identifier: String) -> Task? {
+        
+        guard let id = UUID(uuidString: identifier) else {return nil}
+        
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        let predicate = NSPredicate(format: "identifier == %@", id as NSUUID)
+        fetchRequest.predicate = predicate
+        
+        do {
+            let moc = CoreDataStack.shared.mainContext
+            return try moc.fetch(fetchRequest).first
+        } catch {
+            NSLog("Error fetching task with UUID: \(identifier) :\(error)")
+            return nil
+        }
+    }
 }
 
-    // MARK: Extension
+    // MARK: - Extension
 
 extension TaskController {
     
     static let baseURL = URL(string: "https://tasks-3f211.firebaseio.com/")!
     typealias CompletionHandler = (Error?) -> Void
+    
+    // MARK: - Fetch
     
     func fetchTasksFromServer(completion: @escaping CompletionHandler = { _ in }) {
         
@@ -73,7 +101,14 @@ extension TaskController {
                 let tasRepresentations = try JSONDecoder().decode([String: TaskRepresentation].self, from: data).map({ $0.value })
                 
                 for taskRep in tasRepresentations {
-                    let _ = Task(taskRepresentation: taskRep)
+                    
+                    guard let priority = TaskPriority(rawValue: taskRep.priority) else {return}
+                    
+                    if let task = self.task(for: taskRep.identifier) {
+                        self.updateTask(task: task, name: taskRep.name, notes: taskRep.notes, priority: priority)
+                    } else {
+                        let _ = Task(taskRepresentation: taskRep)
+                    }
                 }
                 
                 self.saveToPersistentStore()
@@ -84,6 +119,44 @@ extension TaskController {
                 return
             }
             
+        }.resume()
+    }
+    
+    // MARK: - Put
+    
+    func put(task: Task, compeltion: @escaping CompletionHandler = { _ in }) {
+        
+        let identifier = task.identifier ?? UUID()
+        
+        let requestURL = TaskController.baseURL.appendingPathComponent(identifier.uuidString).appendingPathExtension("json")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "PUT"
+        
+        do {
+            guard var taskRepresentation = task.taskRespresentation else {
+                compeltion(NSError())
+                return
+            }
+            
+            taskRepresentation.identifier = identifier.uuidString
+            task.identifier = identifier
+            self.saveToPersistentStore()
+            
+            request.httpBody = try JSONEncoder().encode(taskRepresentation)
+        } catch {
+            NSLog("Error encoding task representation: \(error)")
+            compeltion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error putting task: \(error)")
+                compeltion(error)
+                return
+            }
+            compeltion(nil)
         }.resume()
     }
 }

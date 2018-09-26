041c8654ce132a0aeb69838e6e2b8f6380f4343a
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-26 12:45:50 -0500

    NSFetchedResultsControllerDelegate functions completed

--- TasksSC/View Controller/TasksTableViewController.swift
+++ TasksSC/View Controller/TasksTableViewController.swift
@@ -26,29 +26,47 @@ class TasksTableViewController: UITableViewController, NSFetchedResultsControlle
         super.viewWillAppear(animated)
         tableView.reloadData()
     }
+    
+    // MARK: Fetched Results Controller
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<Task> = {
+        
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        let moc = CoreDataStack.shared.mainContext
+        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: "priority", cacheName: nil)
+        
+        frc.delegate = self
+        try! frc.performFetch()
+        return frc
+    }()
+    
 
     // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 1
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return taskController.tasks.count
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath)
-        let task = taskController.tasks[indexPath.row]
+        let task = fetchedResultsController.object(at: indexPath)
 
         cell.textLabel?.text = task.name
         cell.detailTextLabel?.text = task.notes
 
         return cell
     }
-
-
-    // MARK: - Override to support editing the table view
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            let task = taskController.tasks[indexPath.row]
+            let task = fetchedResultsController.object(at: indexPath)
             taskController.deleteTask(task: task)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
@@ -66,26 +84,58 @@ class TasksTableViewController: UITableViewController, NSFetchedResultsControlle
             let destVC = segue.destination as! TaskDetailViewController
             
             guard let indexPath = tableView.indexPathForSelectedRow else { return }
-            let task = taskController.tasks[indexPath.row]
+            let task = fetchedResultsController.object(at: indexPath)
             
             destVC.task = task
             destVC.taskController = taskController
         }
     }
     
-    // MARK: Fetched Results Controller
+    // MARK: - NSFetchedResultsControllerDelegate
     
-    var lazy fetchedResultsController: NSFetchedResultsController<Task> = {
-        
-        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
-        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: true)
-        fetchRequest.sortDescriptors = [sortDescriptor]
-        
-        let moc = CoreDataStack.shared.mainContext
-        let frc = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: "priority", cacheName: nil)
-        
-        frc.delegate = self
-        try! frc.performFetch()
-        return frc
-    }()
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            guard let newIndexPath = newIndexPath else {return}
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        case .update:
+            guard let indexPath = indexPath else {return}
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        case .delete:
+            guard let indexPath = indexPath else {return}
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        case .move:
+            guard let indexPath = indexPath,
+                  let newIndexPath = newIndexPath else {return}
+//            tableView.moveRow(at: indexPath, to: newIndexPath)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange sectionInfo: NSFetchedResultsSectionInfo,
+                    atSectionIndex sectionIndex: Int,
+                    for type: NSFetchedResultsChangeType) {
+        switch type {
+        case .insert:
+            tableView.insertSections(IndexSet(integer: sectionIndex), with: .automatic)
+        case .delete:
+            tableView.deleteSections(IndexSet(integer: sectionIndex), with: .automatic)
+        default:
+            break
+        }
+    }
 }

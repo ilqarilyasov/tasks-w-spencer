00dab300ff9fb82ddd7e8516ac248511e9ca1021
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-25 20:47:15 -0500

    Initial commit

---
+++ TasksSC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TasksSC
+//
+//  Created by Ilgar Ilyasov on 9/25/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

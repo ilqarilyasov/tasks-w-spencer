c50be2bee7fabbe79ff4c41c2ca88b0ea4a3d61e
Ilqar Ilyasov <imilqar@gmail.com>
2018-09-27 12:34:13 -0500

    CoreDataStack changed to ParentChild moc

--- TasksSC/Model/CoreDataStack.swift
+++ TasksSC/Model/CoreDataStack.swift
@@ -13,6 +13,22 @@ class CoreDataStack {
     
     static let shared = CoreDataStack()
     
+    // Will use this instead of saveToPersistence()
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        
+        var error: Error? // For thows function will need an optional Error
+        
+        // This performs given code inside the context's queue
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        if let error = error { throw error }
+    }
+    
     lazy var container: NSPersistentContainer = {
         
         let container = NSPersistentContainer(name: "TasksSC") // Needs to match up with data model file name
@@ -21,6 +37,7 @@ class CoreDataStack {
                 fatalError("Failed to load persistenet stores: \(error)")
             }
         }
+        container.viewContext.automaticallyMergesChangesFromParent = true // Parent - Child
         return container
     }()
     
